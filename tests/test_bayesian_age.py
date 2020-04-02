ae5b7fef001a98f894968adbe62a5c89fc6f6240
Rocio Kiman <rociokiman@gmail.com>
2020-04-02 16:28:26 -0400

    Updated test to accound for delta_m

--- tests/test_bayesian_age.py
+++ tests/test_bayesian_age.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from wdwarfdate.bayesian_age import ln_posterior_prob, get_isochrone_model, get_cooling_model
+from wdwarfdate.bayesian_age import ln_posterior_prob, get_isochrone_model
+from wdwarfdate.bayesian_age import get_cooling_model
 import numpy as np
 
 def test_ln_posterior_prob():
@@ -21,11 +22,12 @@ def test_ln_posterior_prob():
 
     models0 = [ifmr_model,isochrone_model,cooling_models,fig_name,
                dist_file_name]
-    params = ln_ms_age0,ln_cooling_age0
-    params_off = 8.,ln_cooling_age0
+    params = ln_ms_age0,ln_cooling_age0,0
+    params_off = 8.,ln_cooling_age0,0
     
     post_true = ln_posterior_prob(params,teff0,e_teff0,logg0,e_logg0,models0)
-    post_off = ln_posterior_prob(params_off,teff0,e_teff0,logg0,e_logg0,models0)
+    post_off = ln_posterior_prob(params_off,teff0,e_teff0,logg0,e_logg0,
+                                 models0)
     
     assert(post_true > post_off)
     
