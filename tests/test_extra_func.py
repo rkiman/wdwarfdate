ce07fdcdb811270797bfb939ec8aa13d317ceac8
Rocio Kiman <rociokiman@gmail.com>
2020-04-29 11:39:08 -0400

    added test for the calc percentile function

---
+++ tests/test_extra_func.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from src.extra_func import calc_percentiles
+import numpy as np
+
+def test_calc_percentiles():
+    
+    dist = np.random.normal(10,5,1000)
+    log_dist = np.log10(dist)
+    high_perc, low_perc = 84,16
+    results = calc_percentiles(log_dist, log_dist, log_dist, dist, 
+                               dist, high_perc, low_perc, datatype='yr')
+    assert np.isclose(results[0],10,atol=1e-1)
+    assert np.isclose(results[3],10,atol=1e-1)
+    assert np.isclose(results[6],10,atol=1e-1)
+    
+
+
+
