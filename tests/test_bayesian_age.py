19a7d2b7733d8bb41c3781ec3a5090181f2ca541
Rocio Kiman <rociokiman@gmail.com>
2020-07-04 20:05:23 -0400

    Updated tests

--- tests/test_bayesian_age.py
+++ tests/test_bayesian_age.py
@@ -17,11 +17,9 @@ def test_ln_posterior_prob():
     ifmr_model = 'Cummings_2018_MIST'
     isochrone_model = get_isochrone_model(feh='p0.00',vvcrit='0.0')
     
-    fig_name = 'run_' 
-    dist_file_name = 'None'
+    wd_path_id = 'run_' 
 
-    models0 = [ifmr_model,isochrone_model,cooling_models,fig_name,
-               dist_file_name]
+    models0 = [ifmr_model,isochrone_model,cooling_models,wd_path_id]
     params = ln_ms_age0,ln_cooling_age0,0
     params_off = 8.,ln_cooling_age0,0
     
--- tests/test_extra_func.py
+++ tests/test_extra_func.py
@@ -6,14 +6,14 @@ import numpy as np
 
 def test_calc_percentiles():
     
-    dist = np.random.normal(10,5,1000)
+    dist = np.random.normal(20,5,1000)
     log_dist = np.log10(dist)
     high_perc, low_perc = 84,16
     results = calc_percentiles(log_dist, log_dist, log_dist, dist, 
                                dist, high_perc, low_perc, datatype='yr')
-    assert np.isclose(results[0],10,atol=.5)
-    assert np.isclose(results[3],10,atol=.5)
-    assert np.isclose(results[6],10,atol=.5)
+    assert np.isclose(results[0],20,atol=.5)
+    assert np.isclose(results[3],20,atol=.5)
+    assert np.isclose(results[6],20,atol=.5)
     
 
 
