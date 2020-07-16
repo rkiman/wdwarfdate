0a0d016148f7ad8eff46a07bc1b156726b373cc6
Rocio Kiman <rociokiman@gmail.com>
2020-07-16 18:32:02 -0400

    Changed labels corner plot and some cleaning

--- wdwarfdate/bayesian_run_mcmc.py
+++ wdwarfdate/bayesian_run_mcmc.py
@@ -5,8 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import emcee
 import corner
-#import os
-from .bayesian_age import ln_posterior_prob#, ln_prior
+from .bayesian_age import ln_posterior_prob
 from .check_convergence import calc_auto_corr_time
 from .cooling_age import calc_cooling_age
 from .ms_age import calc_ms_age
@@ -80,13 +79,6 @@ def run_mcmc(teff0, e_teff0, logg0, e_logg0, models0,
             plt.grid()
             plt.savefig(wd_path_id+'_corr_time.png')
             plt.close()
-        #Reset sampler and remove the save likelihood evaluation that were saved
-        #while calculating autocorrelation time and burn in
-        #save_likelihoods_file = wd_path_id +'.txt'
-        #os.remove(save_likelihoods_file)
-        
-        #Run mcmc to calculate parameters
-        #p,_,_ = sampler.run_mcmc(p, n_indep_samples*autoc_time);
         
         #Obtain chain of samples
         chain = sampler.chain[:,:,:]
@@ -113,23 +105,19 @@ def plot_results_mcmc(chain,ndim,wd_path_id):
     for i in range(50):
         ax3.plot(chain[i,:,2],color='k',alpha=0.05)
         ax3.axhline(y=np.median(flat_samples[:,2]),color='k')
-    ax3.set_ylabel(r'$delta_m$')
+    ax3.set_ylabel(r'$delta_{\rm m}$')
     plt.tight_layout()
     plt.savefig(wd_path_id + '_walkers.png')
     plt.close(f)
     
-    labels=[r'$\log_{10}($msa$/yr)$',r'$\log_{10}($ca$/yr)$',r'$delta_m$']
+    labels=[r'$\log_{10}($t_{\rm ms}$/yr)$',r'$\log_{10}($t_{\rm cool}$/yr)$',
+            r'$\Delta _{\rm m}$']
     
     fig = corner.corner(flat_samples, labels=labels, 
                         quantiles=[.16,.50,.84], 
                         show_titles=True, title_kwargs={"fontsize": 12})
     fig.savefig(wd_path_id + '_corner_plot.png',dpi=300)
     plt.close(fig)
-    
-    
-def kl_divergence(p, q):
-    mask = (q != 0.0) * (p != 0.0)
-    return np.sum(p[mask] * np.log(p[mask] / q[mask]))
 
     
 def get_initial_conditions(teff0,logg0,model_wd,model_ifmr,
@@ -146,46 +134,3 @@ def get_initial_conditions(teff0,logg0,model_wd,model_ifmr,
                             0])
     
     return init_params
-    '''
-    Creates a grid of main sequence age and cooling age and evaluates the
-    posterior on that grid. Also from the grid gets the distribution of 
-    posterior for the two parameters: main sequence age and cooling age.
-    
-    #Set up grid
-    Ntot = n*n
-    x = np.ones(Ntot)*np.nan
-    y = np.ones(Ntot)*np.nan
-
-    x0 = np.linspace(4,13,n)
-    y0 = np.linspace(4,13,n)
-
-    for ii in range(n):
-        x[ii*n:(ii+1)*n] = np.ones(n)*x0[ii]
-        y[ii*n:(ii+1)*n] = y0
-    
-    #Evaluate posterior on grid    
-    post_list = []
-    for x_i,y_i in zip(x,y):
-        post = ln_posterior_prob([x_i,y_i,0], #set delta_m = 0 
-                                 teff0,e_teff0,logg0,e_logg0,models0)
-        post_list.append(post)
-    post_list = np.array(post_list)
-
-    #Colapse grid to obtain distributions of posterior in main sequence age and
-    #cooling age dimensions
-    post_ms_age = np.array([sum(np.exp(post_list[x==x0_i])) for x0_i in x0])
-    post_cooling_age = np.array([sum(np.exp(post_list[y==y0_i])) for y0_i in y0])
-    
-    #Normalize distribution so the sum is 1
-    norm_post_ms = sum(post_ms_age)*(x0[1]-x0[0])
-    norm_post_cooling = sum(post_cooling_age)*(y0[1]-y0[0])
-    post_ms_age = post_ms_age/norm_post_ms
-    post_cooling_age = post_cooling_age/norm_post_cooling
-
-    axis = x0, y0, x, y
-    
-    idx = np.argmax(post_list)
-    init_max_like = np.array([x[idx],y[idx],0])
-    
-    return axis, post_list, post_ms_age, post_cooling_age, init_max_like
-    '''
