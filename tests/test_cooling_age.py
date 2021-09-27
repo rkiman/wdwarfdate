233883d5f7f58b356d851ae23eea558a189ca896
Rocio Kiman <rociokiman@gmail.com>
2021-09-27 13:58:14 -0700

    Modified old test according to new code

--- tests/test_cooling_age.py
+++ tests/test_cooling_age.py
@@ -4,14 +4,15 @@
 from wdwarfdate.cooling_age import calc_cooling_age
 import numpy as np
 
-def test_calc_cooling_age():
-    N = 100
-    teff_d = np.array([np.random.normal(10000,100,N)])
-    logg_d = np.array([np.random.normal(8,0.1,N)])
-    cooling_age_d,final_mass_d = calc_cooling_age(teff_d,logg_d,1,'DA')
-    assert len(cooling_age_d[0]) == N
-    assert len(final_mass_d[0]) == N
 
-    cooling_age_d2,final_mass_d2 = calc_cooling_age(teff_d,logg_d,1,'DB')
-    assert len(cooling_age_d2[0]) == N
-    assert len(final_mass_d2[0]) == N
+def test_calc_cooling_age():
+    n = 100
+    teff_d = np.array([np.random.normal(10000, 100, n)])
+    logg_d = np.array([np.random.normal(8, 0.1, n)])
+    cooling_age_d, final_mass_d = calc_cooling_age(teff_d, logg_d, 1, 'DA')
+    assert len(cooling_age_d[0]) == n
+    assert len(final_mass_d[0]) == n
+
+    cooling_age_d2, final_mass_d2 = calc_cooling_age(teff_d, logg_d, 1, 'DB')
+    assert len(cooling_age_d2[0]) == n
+    assert len(final_mass_d2[0]) == n
--- tests/test_extra_func.py
+++ tests/test_extra_func.py
@@ -4,17 +4,14 @@
 from wdwarfdate.extra_func import calc_percentiles
 import numpy as np
 
+
 def test_calc_percentiles():
     
-    dist = np.random.normal(20,5,1000)
+    dist = np.random.normal(20, 5, 1000)
     log_dist = np.log10(dist)
-    high_perc, low_perc = 84,16
+    high_perc, low_perc = 84, 16
     results = calc_percentiles(log_dist, log_dist, log_dist, dist, 
-                               dist, high_perc, low_perc, datatype='log')
-    assert np.isclose(results[0],np.log10(20),atol=.5)
-    assert np.isclose(results[3],np.log10(20),atol=.5)
-    assert np.isclose(results[6],np.log10(20),atol=.5)
-    
-
-
-
+                               dist, high_perc, low_perc)
+    assert np.isclose(results[0], np.log10(20), atol=.5)
+    assert np.isclose(results[3], np.log10(20), atol=.5)
+    assert np.isclose(results[6], np.log10(20), atol=.5)
