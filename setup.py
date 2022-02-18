d77fc10534dbc15400c993f46955c7ec23d16a4c
Rocio Kiman <rociokiman@gmail.com>
2022-02-18 15:42:38 -0800

    Updated setup

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup
 
 setup(name='wdwarfdate_code',
       version="0.1.0",
-      description='Calculates white dwarfs ages from Gaia photometry.',
+      description='Calculates white dwarfs ages from teff and logg.',
       url='https://github.com/rkiman/wdwarfdate',
       author='Rocio Kiman',
       author_email='rociokiman@gmail.com',
       #license='MIT',
       packages=['wdwarfdate'],
-      install_requires=['numpy','astropy','matplotlib','emcee','corner','scipy'],
+      install_requires=['numpy','astropy','matplotlib','scipy'],
       zip_safe=False,
       include_package_data=True)
 
